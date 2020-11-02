6b3d0fd3c22954530945776f23e9ca10f6542135
yhzbaba <2679122209@qq.com>
2020-11-02 12:40:20 +0000

    20201102Third

---
+++ include/pmdOptions.hpp
@@ -0,0 +1,86 @@
+/*
+ * ==================================================================
+ * 
+ * Filename: pmdOptions.hpp
+ * 
+ * Created: 10/27/2020 16:34 PM
+ * 
+ * Author: Yuan Haizhuo
+ * 
+ * ==================================================================
+ */
+#ifndef PMDOPTIONS_HPP__
+#define PMDOPTIONS_HPP__
+
+#include "core.hpp"
+#include <boost/program_options.hpp>
+#include <boost/program_options/parsers.hpp>
+
+using namespace std;
+namespace po = boost::program_options;
+
+#define PMD_OPTION_HELP "help"
+#define PMD_OPTION_DBPATH "dbpath"
+#define PMD_OPTION_SVCNAME "svcname" // 监听的端口
+#define PMD_OPTION_MAXPOOL "maxpool"
+#define PMD_OPTION_LOGPATH "logpath"
+#define PMD_OPTION_CONFPATH "confpath"
+
+#define PMD_ADD_PARAM_OPTIONS_BEGIN(desc) \
+  desc.add_options()
+
+#define PMD_ADD_PARAM_OPTIONS_END ;
+
+#define PMD_COMMANDS_STRING(a, b) (string(a) + string(b)).c_str()
+
+#define PMD_COMMANDS_OPTIONS \
+  (PMD_COMMANDS_STRING(PMD_OPTION_HELP, ",h"), "help")(PMD_COMMANDS_STRING(PMD_OPTION_DBPATH, ",d"), boost::program_options::value<string>(), "database file full path")(PMD_COMMANDS_STRING(PMD_OPTION_SVCNAME, ",s"), boost::program_options::value<string>(), "local service name")(PMD_COMMANDS_STRING(PMD_OPTION_MAXPOOL, ",m"), boost::program_options::value<unsigned int>(), "max pooled agent")(PMD_COMMANDS_STRING(PMD_OPTION_LOGPATH, ",l"), boost::program_options::value<string>(), "diagnostic log file full path")(PMD_COMMANDS_STRING(PMD_OPTION_CONFPATH, ",c"), boost::program_options::value<string>(), "configuration file full path")
+
+#define CONFFILENAME "edb.conf"
+#define LOGFILENAME "diag.log"
+#define DBFILENAME "edb.data"
+#define SVCNAME "58127"
+#define NUMPOOL 20
+
+class pmdOptions
+{
+public:
+  pmdOptions();
+  ~pmdOptions();
+
+  int readCmd(int argc, char **argv, po::options_description &desc, po::variables_map &vm);
+  int importVM(const po::variables_map &vm, bool isDefault = true);
+  int readConfigureFile(const char *path, po::options_description &desc, po::variables_map &vm);
+  int init(int argc, char **argv);
+
+  // inline functions
+  inline char *getDBPath()
+  {
+    return _dbPath;
+  }
+  inline char *getLogPath()
+  {
+    return _logPath;
+  }
+  inline char *getConfPath()
+  {
+    return _confPath;
+  }
+  inline char *getServiceName()
+  {
+    return _svcName;
+  }
+  inline int getMaxPool()
+  {
+    return _maxPool;
+  }
+
+private:
+  char _dbPath[OSS_MAX_PATHSIZE + 1];
+  char _logPath[OSS_MAX_PATHSIZE + 1];
+  char _confPath[OSS_MAX_PATHSIZE + 1];
+  char _svcName[NI_MAXSERV + 1];
+  int _maxPool;
+};
+
+#endif
